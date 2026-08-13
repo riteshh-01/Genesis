c41e856906dad3db4dfaa03016c4b393e59575e2
riteshh <riteshh00005@gmail.com>
2026-08-13 09:54:57 +0530

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int arr[5]= {2,6,3,9,1};
+    int ans= INT_MAX;
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]<ans)
+        {
+            ans=arr[i];
+        }
+    }
+    cout<<ans<<endl;
+
+    ans = INT_MIN;
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]>ans)
+        {
+            ans=arr[i];
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
