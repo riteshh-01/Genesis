9faa7d197dc5bba4ad66dbb00a6a1cd1c4637e66
riteshh <riteshh00005@gmail.com>
2026-08-19 20:53:40 +0530

    Add files via upload

---
+++ 26.cpp
@@ -0,0 +1,21 @@
+// Fibbonacci Series
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int arr[100];
+
+    arr[0]=0;
+    arr[1]=1;
+
+    for(int i=2;i<=n-1;i++)
+    arr[i]=arr[i-1]+arr[i-2];
+
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+
+}
